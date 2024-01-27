397c17786944fc41dffc0f737b55017324cbf74a
Banke Bihari <103239794+bankebihari@users.noreply.github.com>
2024-01-27 19:20:43 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const  accontId=12343 //const variables are fixed
+let accountEmail="sunny@gmial.com"
+var accountPassword= "1244"
+accountCity = "patna"
+
+// accontId=2
+// this is not allowed to written 
+
+accountEmail="rahul@gmail.com"
+accountPassword="4332"
+accountCity="chapra"
+
+
+console.log(accontId);
+console.table([accontId,accountEmail,accountPassword,accountCity]);
+/*
+prefer not to use var because of issue in block and functional scope
+*/
