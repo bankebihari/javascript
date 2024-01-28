045a93b5f1951d73de2cfad6492ab9ac320c64a5
Banke Bihari <103239794+bankebihari@users.noreply.github.com>
2024-01-28 16:10:39 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert ("hello") //we are using nodejs, not browser
+
+console.log(3+3); console.log("sunny") // ple take care of readability of code is maintained
+
+let name ="hitesh"
+let age =23
+let isLong= false
+
+// number => 2 to  53
+// bigint
+// string => " ..."
+// boolean => true / false
+// null => standaloe value 
+// undefined 
+// symbol =>unique 
+
+//***  object
+console.log(typeof undefined);  //undefined
+console.log(typeof null);    // object
