7483732d0f8d0b5bc0ab3e9d514459c79fde0f12
Banke Bihari <103239794+bankebihari@users.noreply.github.com>
2024-02-19 10:08:56 +0000

    loop ,array

---
+++ array.js
@@ -0,0 +1,5 @@
+//array
+
+const arr=[12,34,5,3452,33,55,2,51];
+console.log(arr);
+console.log(typeof arr);
---
+++ loop.js
@@ -0,0 +1,96 @@
+// loop
+//=> we can only use the let scope variable during the looop
+
+// for(var i=0;i<=5;i++){
+//     console.log("hello sir")
+// }
+
+
+// calculate the sum of 1 to 5
+//  let sum=0;
+// var n=100
+//  for(let i=0;i<=n;i++){
+//     sum=sum+i
+//  }
+//  console.log(sum);
+
+// // loop count
+// for(let i=0;i<5;i++){
+//    console.log(i);
+// }
+
+//while loop
+//do while
+
+
+
+//for of loop
+// let str="students"
+
+// for(let i of str){
+//    console.log("i :",i);
+// }
+
+
+
+//for in return the key value
+// let student ={
+//    name:"sunny",
+//    age:20,
+//    cgpa:9.0,
+//    ispass:true,
+// }
+// for(let key in student){
+//    console.log(key, student[key])
+// }
+
+// Q1. print all even number from 0 to 100
+// for(let i=1;i<=100;i++){
+//    if(i%2===0){
+//       console.log(i);
+//    }
+// }
+
+
+//  q2. create a game where you start with any random game number .ask the user to keep 
+//guessing the game number until the user enter correct value;
+
+// let gameNum =25;
+// var userNum = prompt("you entered wrong number ! guess the number again")
+
+// while(userNum != gameNum){
+//    userNum = prompt(" you entered the wrog number  !  guess again");
+
+// }
+// console.log("congratulation , you entered the right number");
+
+
+
+//string both are right
+// let str="students"
+// let str='students'
+
+//console.log(str.length);
+
+// let str="myschool"
+// console.log(str[2]);
+
+
+
+
+// templete literal => we try to write the string and varianle both assame time 
+//using backtick key 
+// let obj={
+//    item: "pen",
+//    price: 20,
+
+// };
+// let output= `the cost of ${obj.item} is ${obj.price} .`;
+// console.log(output);
+
+// //console.log("the cost of " , obj.item), "is", obj.price, "rupees",);
+
+// console.log(typeof output);\\
+
+
+
